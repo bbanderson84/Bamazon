367a2f6eeb41e08862fe8e49151029135b5507ad
bbanderson84 <bbanderson84@gmail.com>
2019-03-13 13:41:40 -0600

    updated

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,6 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
+
 
 var connection = mysql.createConnection({
 
@@ -18,7 +20,27 @@ var connection = mysql.createConnection({
   connection.connect(function(error) {
       if (error) throw error;
       console.log("Connected as id " + connection.threadId);
-      // CALL FUNCTIONS
+      start();
   });
 
-  
+  function start() {
+    var query = "SELECT item_id, product_name, price FROM products";
+    connection.query(query, function(error, response) {
+        for (var i = 0; i < response.length; i++) {
+            console.log("Item Id: " + response[i].item_id + " || Product Name: " + response[i].product_name  + " || Price: " + response[i].price);
+        }
+
+        itemSearch();
+    });
+  }
+
+  function itemSearch() {
+      inquirer
+      .prompt([
+        {
+          name: "buyItem",
+          type: "input",
+          message: "What is the ID # of the item you would like to purchase?",
+        }
+    ])
+} 
